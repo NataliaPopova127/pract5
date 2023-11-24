ee8d647ee1711958fcd09306b807794bb31dd196
NataliaPopova127 <pnatala127@mail.ru>
2023-11-24 23:51:34 +0300

    f

---
+++ pract5/LineiPage.xaml.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace pract5
+{
+    /// <summary>
+    /// Логика взаимодействия для LineiPage.xaml
+    /// </summary>
+    public partial class LineiPage : Page
+    {
+        public LineiPage()
+        {
+            InitializeComponent();
+        }
+
+        private void btnRes_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                var x = Convert.ToDouble(tbx.Text);
+                var y = Convert.ToDouble(tby.Text);
+                var z = Convert.ToDouble(tbz.Text);
+                double res = 0;
+
+                if (Math.Pow(x, 2) + Math.Pow(y, 2) + 2 == 0)
+                {
+                    MessageBox.Show("Ошибка, знаменатель меньше нуля!");
+                }
+                else
+                {
+                    res = Math.Sqrt(Math.Pow(8 + Math.Pow(Math.Abs(x - y) + 1, 2), 1/3)) / (Math.Pow(x, 2) + Math.Pow(y, 2) + 2) - Math.Pow(Math.E,Math.Abs(x-y)) * Math.Pow(Math.Pow(Math.Tan(z),2) + 1,x);
+                    tbResult.Text = res.ToString();
+                }
+            }
+            catch(Exception ex)
+            {
+                MessageBox.Show("Ошибка: " + ex.Message);
+            }
+        }
+    }
+}
---
+++ pract5/VetvlPage.xaml.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace pract5
+{
+    /// <summary>
+    /// Логика взаимодействия для VetvlPage.xaml
+    /// </summary>
+    public partial class VetvlPage : Page
+    {
+        public VetvlPage()
+        {
+            InitializeComponent();
+        }
+
+        private void btnRes_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
