110ffd60afe5374efa8aac5e8f0c907c674d2fc8
NataliaPopova127 <pnatala127@mail.ru>
2023-11-27 12:41:57 +0300

    add 2 form

--- pract5/VetvlPage.xaml.cs
+++ pract5/VetvlPage.xaml.cs
@@ -27,7 +27,60 @@ namespace pract5
 
         private void btnRes_Click(object sender, RoutedEventArgs e)
         {
+            try
+            {
+                var x = Convert.ToDouble(tbx.Text);
+                var y = Convert.ToDouble(tby.Text);
+                var result = 0.0;
+                var f = 0.0;
 
+                if (rbCos.IsChecked == true)
+                {
+                    f = Math.Cos(x);
+                }
+                else if (rbExp.IsChecked == true)
+                {
+                    f = Math.Exp(x);
+                }
+                else if (rbSqr.IsChecked == true)
+                {
+                    f = Math.Sqrt(x);
+                }
+                else
+                {
+                    throw new Exception("Выберите функцию");
+                }
+                if(x * y > 0)
+                {
+                    result = (f + y) * (f + y);
+                }
+                else if (x * y < 0)
+                {
+                    result = f * Math.Cos(y);
+                }
+                else if (x * y == 0)
+                {
+                    result = f + y;
+                }
+
+                tbResult.Text = result.ToString();
+            }
+            catch(Exception ex)
+            {
+                MessageBox.Show("Ошибка: "+ ex.Message);
+            }
+        }
+
+        private void cbColor_Checked(object sender, RoutedEventArgs e)
+        {
+            if(cbColor.IsChecked == true)
+            {
+                tbResult.Foreground = Brushes.Red;
+            }
+            else
+            {
+                tbResult.Foreground = Brushes.Black;
+            }
         }
     }
 }
